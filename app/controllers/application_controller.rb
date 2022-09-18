121edc6cf83d3f61e27ecba226dec7ed0955b5dc
galiyafatt2 <elizabeth.mor324@gmail.com>
2022-09-18 19:27:28 +0300

    devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,15 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
   after_action :request_response
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def logged_in?
-    !!current_user
-  end
+  protected
 
-  def require_user
-    unless logged_in?
-      flash[:danger] = 'You must be logged in to perform that action'
-      redirect_to root_path
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:email, :username, :password)
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#index'
   get 'about', to:
     'pages#about'
   resources :articles
   get 'signup', to: 'users#new'
+  get 'users/signup', to: 'users#new'
   resources :users, except: [:new]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
   delete 'logout', to: 'sessions#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
